05a813ed37e854a81852f0158a97035818614591
karyosome1 <87289805+karyosome1@users.noreply.github.com>
2025-06-27 11:06:33 +0900

    [level 2] Title: 괄호 회전하기, Time: 21.06 ms, Memory: 83.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String s) {
+        int n = s.length();
+        int count = 0;
+        
+        for (int i = 0; i < n; i++) {
+            String rotated = s.substring(i) + s.substring(0, i);
+            if (isValid(rotated)) {
+                count++;
+            }
+        }
+        return count;
+    }        
+    private boolean isValid(String s) {
+        Deque<Character> stack = new ArrayDeque<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) return false;
+                char top = stack.pop();
+                if ((c == ')' && top != '(') || (c == ']' && top != '[') || (c == '}' && top != '{')) {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+
+    }
+}
