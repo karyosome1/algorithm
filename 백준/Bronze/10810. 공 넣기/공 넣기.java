977adce80cdcf8af4c8e1c8c56bdcb9829e6ec52
karyosome1 <87289805+karyosome1@users.noreply.github.com>
2025-07-11 08:26:12 +0900

    [Bronze III] Title: 공 넣기, Time: 120 ms, Memory: 15860 KB -BaekjoonHub

---
+++ 백준/Bronze/10810. 공 넣기/공 넣기.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[] baskets = new int[n];
+
+        for (int t = 0; t < m; t++) {
+            st = new StringTokenizer(br.readLine());
+            int i = Integer.parseInt(st.nextToken());
+            int j = Integer.parseInt(st.nextToken());
+            int k = Integer.parseInt(st.nextToken());
+
+            for (int idx = i - 1; idx < j; idx++) {
+                baskets[idx] = k;
+            }
+        }
+        for (int val : baskets) {
+            System.out.print(val + " ");
+        }
+    }
+}
