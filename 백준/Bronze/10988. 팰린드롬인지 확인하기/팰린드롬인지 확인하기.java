6cd4d405ca78cd4d42bd9c1faa7343eb9ae0582a
karyosome1 <87289805+karyosome1@users.noreply.github.com>
2025-08-01 11:16:14 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 104 ms, Memory: 14196 KB -BaekjoonHub

---
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+
+        boolean isPalindrome = true;
+        int len = s.length();
+        for (int i = 0; i < len / 2; i++) {
+            if (s.charAt(i) != s.charAt(len - i - 1)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+        System.out.println(isPalindrome ? 1 : 0);
+    }
+}
