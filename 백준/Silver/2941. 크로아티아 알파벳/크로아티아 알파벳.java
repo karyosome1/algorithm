9bc58fbabbb68686418e243c43e6ece8ada21ebb
karyosome1 <87289805+karyosome1@users.noreply.github.com>
2025-08-03 09:13:22 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 104 ms, Memory: 14252 KB -BaekjoonHub

---
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+
+        String[] croatia = {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
+
+        for (String c : croatia) {
+            str = str.replace(c, "*");
+        }
+        System.out.println(str.length());
+    }
+}
