2fc6332144039dc7e5d6584b3b2184e01492d7d5
karyosome1 <87289805+karyosome1@users.noreply.github.com>
2025-08-10 08:40:38 +0900

    [Bronze II] Title: 진법 변환, Time: 104 ms, Memory: 14300 KB -BaekjoonHub

---
+++ 백준/Bronze/2745. 진법 변환/진법 변환.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        String n = st.nextToken();
+        int b = Integer.parseInt(st.nextToken());
+
+        int result = Integer.parseInt(n, b);
+        System.out.println(result);
+    }
+}
