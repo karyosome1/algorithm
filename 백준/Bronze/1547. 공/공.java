844d275ff1ec7149133867dc7d64e3590930acc8
karyosome1 <87289805+karyosome1@users.noreply.github.com>
2025-09-15 10:14:10 +0900

    [Bronze III] Title: 공, Time: 108 ms, Memory: 14136 KB -BaekjoonHub

---
+++ 백준/Bronze/1547. 공/공.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int m = Integer.parseInt(br.readLine());
+        int ball = 1;
+
+        for (int i = 0; i < m; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            if (x == ball) {
+                ball = y;
+            } else if (y == ball) {
+                ball = x;
+            }
+        }
+        System.out.println(ball);
+    }
+}
