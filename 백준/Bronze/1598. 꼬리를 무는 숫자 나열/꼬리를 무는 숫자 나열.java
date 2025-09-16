1c1f86069b93fd401e13168661f636241dfeaccc
karyosome1 <87289805+karyosome1@users.noreply.github.com>
2025-09-16 09:45:48 +0900

    [Bronze III] Title: 꼬리를 무는 숫자 나열, Time: 104 ms, Memory: 14120 KB -BaekjoonHub

---
+++ 백준/Bronze/1598. 꼬리를 무는 숫자 나열/꼬리를 무는 숫자 나열.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken()) - 1;
+        int b = Integer.parseInt(st.nextToken()) - 1;
+
+        int distance = Math.abs(a % 4 - b % 4) + Math.abs(a / 4 - b / 4);
+        System.out.println(distance);
+    }
+}
