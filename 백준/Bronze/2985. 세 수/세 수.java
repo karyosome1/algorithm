c47612c3b6751a7736822d872c589fda666cb477
karyosome1 <87289805+karyosome1@users.noreply.github.com>
2025-11-08 10:07:46 +0900

    [Bronze III] Title: 세 수, Time: 204 ms, Memory: 18764 KB -BaekjoonHub

---
+++ 백준/Bronze/2985. 세 수/세 수.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        if (a + b == c) {
+            System.out.println(a + "+" + b + "=" + c);
+        } else if (a - b == c) {
+            System.out.println(a + "-" + b + "=" + c);
+        } else if (a * b == c) {
+            System.out.println(a + "*" + b + "=" + c);
+        } else if (a % b == 0 && a / b == c) {
+            System.out.println(a + "/" + b + "=" + c);
+        }
+
+        else if (a == b + c) {
+            System.out.println(a + "=" + b + "+" + c);
+        } else if (a == b - c) {
+            System.out.println(a + "=" + b + "-" + c);
+        } else if (a == b * c) {
+            System.out.println(a + "=" + b + "*" + c);
+        } else if (b % c == 0 && b / c == a) {
+            System.out.println(a + "=" + b + "/" + c);
+        }
+    }
+}
