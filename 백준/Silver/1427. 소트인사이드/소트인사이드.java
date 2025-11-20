a5132009ed587f08ba8a00de866fad1356ca0ff0
karyosome1 <87289805+karyosome1@users.noreply.github.com>
2025-11-20 09:18:37 +0900

    [Silver V] Title: 소트인사이드, Time: 172 ms, Memory: 17664 KB -BaekjoonHub

---
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] array = new int[10];
+
+        while (n > 0) {
+            array[n % 10]++;
+            n /= 10;
+        }
+
+        for (int i = array.length - 1; i >= 0; i--) {
+            while(array[i]-- > 0) {
+                System.out.print(i);
+            }
+        }
+    }
+}
