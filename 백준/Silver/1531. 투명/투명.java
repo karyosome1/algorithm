f71c357798fa996223ed0a5ed98d417a46c0b4c8
karyosome1 <87289805+karyosome1@users.noreply.github.com>
2025-11-24 09:23:51 +0900

    [Silver V] Title: 투명, Time: 200 ms, Memory: 18088 KB -BaekjoonHub

---
+++ 백준/Silver/1531. 투명/투명.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        int[][] arr = new int[101][101];
+
+        for (int i = 0; i < n; i++) {
+            int x1 = sc.nextInt();
+            int y1 = sc.nextInt();
+            int x2 = sc.nextInt();
+            int y2 = sc.nextInt();
+
+            for (int j = x1; j <= x2; j++) {
+                for (int k = y1; k <= y2; k++) {
+                    arr[j][k]++;
+                }
+            }
+        }
+
+        int cnt = 0;
+        for (int i = 1; i <= 100; i++) {
+            for (int j = 1; j <= 100; j++) {
+                if (arr[i][j] > m) cnt++;
+            }
+        }
+
+        System.out.println(cnt);
+    }
+}
