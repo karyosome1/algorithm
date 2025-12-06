2a38e8930b0329a9401e60696090c153bc0d32f5
karyosome1 <87289805+karyosome1@users.noreply.github.com>
2025-12-07 08:08:15 +0900

    [Silver V] Title: 삼각형만들기, Time: 404 ms, Memory: 18212 KB -BaekjoonHub

---
+++ 백준/Silver/2622. 삼각형만들기/삼각형만들기.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int cnt = 0;
+        for (int a = 1; a <= n / 3; a++) {
+            for (int b = a; b <= (n - a) / 2; b++) {
+                int c = n - a - b;
+                if (b > c) break;
+                if (c < a + b) {
+                    cnt++;
+                }
+            }
+        }
+        System.out.println(cnt);
+    }
+}
