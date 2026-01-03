e9c2f80ccf269eb7f7e2770c6703105be36e4ea2
karyosome1 <87289805+karyosome1@users.noreply.github.com>
2026-01-03 10:15:16 +0900

    [Silver IV] Title: 한수, Time: 104 ms, Memory: 14148 KB -BaekjoonHub

---
+++ 백준/Silver/1065. 한수/한수.java
@@ -0,0 +1,30 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        if (n < 100) {
+            System.out.println(n);
+            return;
+        }
+
+        if (n == 1000) {
+            n = 999;
+        }
+
+        int count = 99;
+        for (int i = 100; i <= n; i++) {
+            int hundred = i / 100;
+            int ten = (i / 10) % 10;
+            int one = i % 10;
+
+            if (hundred - ten == ten - one) {
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
