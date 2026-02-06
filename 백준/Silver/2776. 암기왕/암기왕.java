85e2fd3bb0e5167795b69f050984ce1da851e26e
karyosome1 <87289805+karyosome1@users.noreply.github.com>
2026-02-06 09:35:46 +0900

    [Silver IV] Title: 암기왕, Time: 1436 ms, Memory: 286976 KB -BaekjoonHub

---
+++ 백준/Silver/2776. 암기왕/암기왕.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int t = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < t; i++) {
+            Set<Integer> set = new HashSet<>();
+            int n = Integer.parseInt(br.readLine());
+
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                set.add(Integer.parseInt(st.nextToken()));
+            }
+
+            int m = Integer.parseInt(br.readLine());
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                if (set.contains(Integer.parseInt(st.nextToken()))) {
+                    sb.append('1').append('\n');
+                } else {
+                    sb.append('0').append('\n');
+                }
+            }
+        }
+
+        System.out.print(sb);
+    }
+}
