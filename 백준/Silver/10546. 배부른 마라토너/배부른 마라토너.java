20765f4f274635e20c2473f6528e5cadec1f1559
karyosome1 <87289805+karyosome1@users.noreply.github.com>
2026-04-19 08:38:26 +0900

    [Silver IV] Title: 배부른 마라토너, Time: 396 ms, Memory: 35516 KB -BaekjoonHub

---
+++ 백준/Silver/10546. 배부른 마라토너/배부른 마라토너.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        Map<String, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            String name = br.readLine();
+            map.put(name, map.getOrDefault(name, 0) + 1);
+        }
+
+        for (int i = 0; i < n - 1; i++) {
+            String name = br.readLine();
+            map.put(name, map.get(name) - 1);
+            if (map.get(name) == 0) {
+                map.remove(name);
+            }
+        }
+
+        for (String name : map.keySet()) {
+            System.out.println(name);
+            break;
+        }
+    }
+}
